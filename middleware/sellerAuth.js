471cdce8e8f6b0080ba2e3630eb7ae6125faed7a
MurtadhaMohammed <mumu9943@gmail.com>
2024-09-17 12:52:42 +0300

    update

--- middleware/sellerAuth.js
+++ middleware/sellerAuth.js
@@ -4,7 +4,7 @@ const JWT_SECRET = process.env.JWT_SECRET; // Replace with your actual secret
 
 const sellerAuth = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader;
+  const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.sendStatus(401);
   jwt.verify(token, JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
