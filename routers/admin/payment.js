6f6d2647616a45aca024e6134b30e08b2729ddd8
MurtadhaMohammed <mumu9943@gmail.com>
2024-09-22 13:03:36 +0300

    update

--- routers/admin/payment.js
+++ routers/admin/payment.js
@@ -36,8 +36,40 @@ router.post("/", sellerAuth, async (req, res) => {
 // Read all Payments
 router.get("/", dashboardAuth, async (req, res) => {
   try {
-    const payments = await prisma.payment.findMany();
-    res.json({ data: payments });
+    // Get the page and limit from query parameters, with default values
+    const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
+    const limit = parseInt(req.query.limit) || 10; // Default to 10 items per page if not provided
+
+    // Calculate the number of items to skip based on the page and limit
+    const skip = (page - 1) * limit;
+
+    // Fetch the total count of records to calculate total pages
+    const totalPayments = await prisma.payment.count();
+
+    // Fetch the payments with pagination
+    const payments = await prisma.payment.findMany({
+      skip: skip, // Skip the previous records
+      take: limit, // Limit the number of records fetched
+      include: {
+        seller: true,
+        provider: true,
+      },
+      orderBy: {
+        createtAt: "desc",
+      },
+    });
+
+    // Calculate total number of pages
+    const totalPages = Math.ceil(totalPayments / limit);
+
+    // Return paginated results
+    res.json({
+      data: payments,
+      totalItems: totalPayments,
+      totalPages: totalPages,
+      currentPage: page,
+      pageSize: limit,
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
