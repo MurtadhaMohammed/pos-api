bc0f11c99483327b55b12b996688d433fa55a449
MurtadhaMohammed <mumu9943@gmail.com>
2024-10-31 16:53:00 +0300

    update

--- routers/admin/admin.js
+++ routers/admin/admin.js
@@ -3,6 +3,7 @@ const prisma = require("../../prismaClient");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
+const adminAuth = require("../../middleware/adminAuth");
 
 const JWT_SECRET = process.env.JWT_SECRET; // Replace with your actual secret
 
@@ -13,6 +14,21 @@ router.post("/register", async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const admin = await prisma.admin.create({
       data: { name, type, username, password: hashedPassword },
+    });
+    res.json(admin);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Register Admin
+router.post("/reset", adminAuth, async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const admin = await prisma.admin.update({
+      where: { username },
+      data: { password: hashedPassword },
     });
     res.json(admin);
   } catch (error) {
