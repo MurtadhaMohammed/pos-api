bac3d5b8e62e3be328ff29006de6fc694501ce2d
Fatima Alaa Muhammed <fatimaalaa2001xx@gmail.com>
2024-12-18 12:01:35 +0300

    add card summary

--- routers/admin/provider.js
+++ routers/admin/provider.js
@@ -163,5 +163,67 @@ router.get("/about/:id", dashboardAuth, async (req, res) => {
 
 });
 
+router.get("/summary/:id", dashboardAuth, async (req, res) => {
+  const  providerId  = req.params.id;
+
+  if (!providerId) {
+    return res.status(400).json({ error: "Provider ID is required" });
+  }
+
+  try {
+    const cards = await prisma.card.findMany({
+      where: {
+        providerId: Number(providerId),
+      },
+      select: {
+        id: true,
+        cardType: {
+          select: {
+            companyCardID: true,
+          },
+        },
+      },
+    });
+
+    const companyCardIds = Array.from(
+      new Set(cards.map((card) => card.cardType.companyCardID))
+    );
+
+    if (companyCardIds.length === 0) {
+      return res.status(404).json({ error: "No cards found for the provider" });
+    }
+
+   const formData = new FormData();
+   formData.append("companyCardIds", JSON.stringify(companyCardIds));
+
+   const response = await fetch(
+     "https://client.nojoomalrabiaa.com/api/v1/client/card-summary", 
+     {
+       method: "POST",
+       headers: {
+         "Authorization": `Bearer ${process.env.COMPANY_TOKEN}`,
+       },
+       body: formData,
+     }
+   );
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      return res.status(response.status).json({ error: `External API error: ${errorText}` });
+    }
+
+    const externalResponseData = await response.json();
+
+    res.status(200).json({
+      message: "Summary fetched successfully",
+      data: externalResponseData,
+    });
+  } catch (error) {
+    console.error("Error fetching card summary:", error.message);
+    res.status(500).json({ error: "An error occurred while fetching the summary" });
+  }
+});
+
+
 
 module.exports = router;
