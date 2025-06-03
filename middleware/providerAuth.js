a6244833d2536907987e557da03d72904bc0be12
Fatima Alaa <91639370+pulloxz@users.noreply.github.com>
2025-06-03 11:10:42 +0300

    edit

--- middleware/providerAuth.js
+++ middleware/providerAuth.js
@@ -6,7 +6,6 @@ const JWT_SECRET = process.env.JWT_SECRET; // Replace with your actual secret
 const providerAuth = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-
   if (!token) return res.sendStatus(401);
 
   jwt.verify(token, JWT_SECRET, async (err, decodedUser) => {
@@ -16,24 +15,26 @@ const providerAuth = async (req, res, next) => {
       }
       return res.sendStatus(403);
     }
+
+    if (!["PROVIDER", "ADMIN"].includes(decodedUser.type)) {
+      return res.status(403).json({ error: "Access denied (invalid role)" });
+    }
+
     try {
       const user = await prisma.admin.findFirst({
-        where: { id: decodedUser.id },
-        include: {
-          provider: true,
+        where: {
+          id: decodedUser.id,
+          type: decodedUser.type, 
         },
+        include: { provider: true },
       });
 
       if (!user) {
         return res.status(404).json({ error: "User not found!" });
       }
 
-      if (user.type === "PROVIDER") {
-        if (!user?.provider || !user?.provider.active) {
-          return res
-            .status(403)
-            .json({ error: "Provider account is not active" });
-        }
+      if (user.type === "PROVIDER" && (!user.provider || !user.provider.active)) {
+        return res.status(403).json({ error: "Provider account is not active" });
       }
 
       req.user = decodedUser;
