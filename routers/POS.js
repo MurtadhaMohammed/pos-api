7366eae99dce5e12a33770926dc294ecbb30501d
MurtadhaMohammed <mumu9943@gmail.com>
2025-06-30 14:24:43 +0300

    update

--- routers/POS.js
+++ routers/POS.js
@@ -1096,12 +1096,12 @@ router.get("/invoice/:id", sellerAuth, async (req, res) => {
             `invoices/${payment?.sellerId}-invoice.png`,
             (err, data) => {
               if (err) {
-                // fs.unlinkSync(`invoices/${payment?.sellerId}-invoice.png`);
+                fs.unlinkSync(`invoices/${payment?.sellerId}-invoice.png`);
                 console.error("Error reading file:", err);
                 res.status(500).json({ message: "Error reading file" });
               } else {
                 const base64Image = data.toString("base64");
-                // fs.unlinkSync(`invoices/${payment?.sellerId}-invoice.png`);
+                fs.unlinkSync(`invoices/${payment?.sellerId}-invoice.png`);
                 res.status(200).json({ image: base64Image });
               }
             }
