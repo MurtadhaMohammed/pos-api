68dda7cbf0a2535d9f681870ce0d5f69fa3e411b
MurtadhaMohammed <mumu9943@gmail.com>
2025-08-06 16:21:01 +0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-var cors = require("cors");
+// var cors = require("cors");
 var cron = require("node-cron");
 const http = require("http");
 const { Server } = require("socket.io");
